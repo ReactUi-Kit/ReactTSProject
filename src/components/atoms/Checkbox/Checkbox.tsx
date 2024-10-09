0c9ca5295c268b4f507216fdde8ab1277389ec03
GoncalvesLionel <lionel78330@gmail.com>
2024-10-09 15:54:02 +0200

    Fix

--- src/components/atoms/Checkbox/Checkbox.tsx
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import { CheckboxProps } from "./Checkbox.props";
 
-export default function Checkbox({ label, options, multiple = true, ...checkboxProps }: CheckboxProps) {
-  if (multiple) {
-    // Si `multiple` est vrai, on rend des checkboxes
-    return (
-      <div>
-        <label>{label}</label>
-        {options.map((option) => (
-          <div key={option.value}>
-            <input
-              type="checkbox"
-              id={option.value}
-              value={option.value}
-              {...(checkboxProps)}            />
-            <label htmlFor={option.value}>{option.label}</label>
-          </div>
-        ))}
-      </div>
-    );
-  }
+export default function Checkbox({ label, options, ...checkboxProps }: CheckboxProps) {
+  return (
+    <div>
+      <label>{label}</label>
+      {options.map((option) => (
+        <div key={option.value}>
+          <input
+            type="checkbox"
+            id={option.value}
+            value={option.value}
+            {...(checkboxProps)}            />
+          <label htmlFor={option.value}>{option.label}</label>
+        </div>
+      ))}
+    </div>
+  );
 }
