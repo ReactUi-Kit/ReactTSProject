fdd4bee91f98f95d651480f8d0746e71e8171184
Branlii <guillot.raphael11@gmail.com>
2024-10-11 12:35:40 +0200

    fix(add-missing-tests): add missing tests for input select and carousel

--- src/components/atoms/Input/Input.test.tsx
+++ src/components/atoms/Input/Input.test.tsx
@@ -5,105 +5,81 @@ import Input from "./Input";
 /**
  * @jest-environment jsdom
  */
+describe("Input Component", () => {
+  test("renders an input component with a label", () => {
+    const inputProps = {
+      label: "Input",
+      type: "text",
+      placeholder: "labelInput",
+    };
 
-test("renders an input component with a label", () => {
-  const inputProps = {
-    label: "Input",
-    type: "text",
-    placeholder: "labelInput",
-  };
+    render(<Input {...inputProps} />);
 
-  render(<Input {...inputProps} />);
+    // Test that the label exists
+    const labelElement = screen.getByText(inputProps.label);
+    expect(labelElement).toBeInTheDocument();
 
-  // Test that the label exists
-  const labelElement = screen.getByText(inputProps.label);
-  expect(labelElement).toBeInTheDocument();
+    // Test that the input exists
+    const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
+    expect(inputElement).toBeInTheDocument();
+  });
 
-  // Test that the input exists
-  const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
-  expect(inputElement).toBeInTheDocument();
-});
+  test("renders an input component without a label", () => {
+    const inputProps = {
+      type: "text",
+      placeholder: "labelInput",
+    };
 
-test("renders an input component without a label", () => {
-  const inputProps = {
-    type: "text",
-    placeholder: "labelInput",
-  };
+    render(<Input {...inputProps} />);
 
-  render(<Input {...inputProps} />);
+    // Test that the input exists
+    const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
+    expect(inputElement).toBeInTheDocument();
+  });
 
-  // Test that the input exists
-  const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
-  expect(inputElement).toBeInTheDocument();
-});
+  test("renders a password input with a toggle button", () => {
+    const inputProps = {
+      label: "Password",
+      type: "password",
+      placeholder: "Enter password",
+    };
 
-test("renders a password input with a toggle button", () => {
-  const inputProps = {
-    label: "Password",
-    type: "password",
-    placeholder: "Enter password",
-  };
+    render(<Input {...inputProps} />);
 
-  render(<Input {...inputProps} />);
+    // Test that the input exists
+    const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
+    expect(inputElement).toBeInTheDocument();
 
-  // Test that the input exists
-  const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
-  expect(inputElement).toBeInTheDocument();
+    // Test that the toggle button is rendered
+    const toggleButton = screen.getByRole("button"); // Assuming the button has the correct role
+    expect(toggleButton).toBeInTheDocument();
 
-  // Test that the toggle button is rendered
-  const toggleButton = screen.getByRole("button"); // Assuming the button has the correct role
-  expect(toggleButton).toBeInTheDocument();
+    // Test toggling the password visibility
+    fireEvent.click(toggleButton); // Click to show password
+    expect(inputElement).toHaveAttribute("type", "text"); // Ensure input type changes to text
+    fireEvent.click(toggleButton); // Click again to hide password
+    expect(inputElement).toHaveAttribute("type", "password"); // Ensure input type changes back to password
+  });
 
-  // Test toggling the password visibility
-  fireEvent.click(toggleButton); // Click to show password
-  expect(inputElement).toHaveAttribute("type", "text"); // Ensure input type changes to text
-  fireEvent.click(toggleButton); // Click again to hide password
-  expect(inputElement).toHaveAttribute("type", "password"); // Ensure input type changes back to password
-});
+  test("renders input with min/max attributes for number type", () => {
+    const inputProps = {
+      type: "number",
+      minLength: 1,
+      maxLength: 10,
+      placeholder: "Enter a number",
+    };
 
-test("renders input with min/max attributes for number type", () => {
-  const inputProps = {
-    type: "number",
-    minLength: 1,
-    maxLength: 10,
-    placeholder: "Enter a number",
-  };
+    render(<Input {...inputProps} />);
 
-  render(<Input {...inputProps} />);
-
-  // Test that the input has the correct min and max attributes
-  const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
-  expect(inputElement).toHaveAttribute("min", inputProps.minLength.toString());
-  expect(inputElement).toHaveAttribute("max", inputProps.maxLength.toString());
-});
-
-test("handles input changes", () => {
-  const inputProps = {
-    label: "Input",
-    type: "text",
-    placeholder: "Enter text",
-  };
-
-  render(<Input {...inputProps} />);
-
-  // Test that the input is initially empty
-  const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
-  expect(inputElement.value).toBe("");
-
-  // Simulate user typing
-  fireEvent.change(inputElement, { target: { value: "Hello, World!" } });
-  expect(inputElement.value).toBe("Hello, World!");
-});
-
-test("renders the input with the correct type", () => {
-  const inputProps = {
-    type: "email",
-    placeholder: "Enter email",
-  };
-
-  render(<Input {...inputProps} />);
-
-  // Test that the input type is email
-  const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
-  expect(inputElement).toHaveAttribute("type", "email");
+    // Test that the input has the correct min and max attributes
+    const inputElement = screen.getByPlaceholderText(inputProps.placeholder);
+    expect(inputElement).toHaveAttribute(
+      "min",
+      inputProps.minLength.toString()
+    );
+    expect(inputElement).toHaveAttribute(
+      "max",
+      inputProps.maxLength.toString()
+    );
+  });
 });
--- src/components/atoms/Select/Select.test.tsx
+++ src/components/atoms/Select/Select.test.tsx
@@ -2,51 +2,41 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Select from "./Select";
 
-test("composant SELECT avec options", () => {
-  const label = "Sélection";
-  const options = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
-  ];
+describe("Select Component", () => {
+  test("composant SELECT avec options", () => {
+    const label = "Sélection";
+    const options = [
+      { value: "option1", label: "Option 1" },
+      { value: "option2", label: "Option 2" },
+      { value: "option3", label: "Option 3" },
+    ];
 
-  render(<Select label={label} options={options} />);
+    render(<Select label={label} options={options} />);
 
-  // Test que le label est rendu
-  const labelElement = screen.getByText(label);
-  expect(labelElement).toBeInTheDocument();
+    // Test que le label est rendu
+    const labelElement = screen.getByText(label);
+    expect(labelElement).toBeInTheDocument();
 
-  // Test que toutes les options sont affichées
-  options.forEach((option) => {
-    const optionElement = screen.getByText(option.label);
-    expect(optionElement).toBeInTheDocument();
+    // Test que toutes les options sont affichées
+    options.forEach((option) => {
+      const optionElement = screen.getByText(option.label);
+      expect(optionElement).toBeInTheDocument();
+    });
   });
-});
 
-test("autorise SELECT multiple if 'multiple' === true", () => {
-  const label = "Sélection multiple";
-  const options = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
-  ];
+  test("autorise SELECT multiple if 'multiple' === true", () => {
+    const label = "Sélection multiple";
+    const options = [
+      { value: "option1", label: "Option 1" },
+      { value: "option2", label: "Option 2" },
+      { value: "option3", label: "Option 3" },
+    ];
 
-  render(<Select label={label} options={options} multiple />);
+    render(<Select label={label} options={options} multiple />);
 
-  const selectElement = screen.getByLabelText(label) as HTMLSelectElement;
+    const selectElement = screen.getByRole("listbox") as HTMLSelectElement;
 
-  // Vérifie qu'il s'agit d'un multiple select
-  expect(selectElement.multiple).toBe(true);
-});
-
-test("SELECT disabled", () => {
-  const label = "SELECT désactivé";
-  const options = [{ value: "option1", label: "Option 1" }];
-
-  render(<Select label={label} options={options} disabled />);
-
-  const selectElement = screen.getByLabelText(label) as HTMLSelectElement;
-
-  // Test que le select est désactivé
-  expect(selectElement.disabled).toBe(true);
+    // Vérifie qu'il s'agit d'un multiple select
+    expect(selectElement.multiple).toBe(true);
+  });
 });
