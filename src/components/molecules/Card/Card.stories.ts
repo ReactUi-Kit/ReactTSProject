467419ad929fbf999759da3026c7f8099cc23636
Matheo <matheoprofessionnel12@gmail.com>
2024-10-11 10:52:45 +0200

    molecule-card-add

--- src/components/molecules/Card/Card.stories.ts
+++ src/components/molecules/Card/Card.stories.ts
@@ -11,7 +11,11 @@ type Story = StoryObj<typeof Card>;
 
 export const AlertStory: Story = {
   args: {
-    
+    title: 'Title',
+    description: 'Description',
+    imageUrl: 'https://images.affiches-et-posters.com/albums/3/56170/affiche-film-joker.jpg',
+    imageAlt: 'joker Meilleur que batman',
+    layout: "column", 
   },
 };
 
